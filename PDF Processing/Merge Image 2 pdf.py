ce7b01d091cd63df3f48cc680f2f4d823e5765c3
pyguru123 <pyguru123@gmail.com>
2021-11-23 23:29:11 +0530

    updated merge 2 pdf

--- PDF Processing/Merge Image 2 pdf.py
+++ PDF Processing/Merge Image 2 pdf.py
@@ -1,9 +1,11 @@
-# /
+# Merge Multiple Images to A4 sized pdf
+# pip install fpdf
 
 from fpdf import FPDF
+
 pdf = FPDF()
-imagelist = [f'pdfs/{i}.jpg' for i in range(1, 7)]
+imagelist = [f'{i}.jpg' for i in range(1, 10)]
 for image in imagelist:
     pdf.add_page()
-    pdf.image(image,0, 0, 210, 297)
-pdf.output("english.pdf", "F")
+    pdf.image(image, 0, 0, 210, 297)
+pdf.output("output.pdf", "F")
